50af80afa962622214d94197f3eb68a525da507a
hadaszm <Ghadasz@interia.eu>
2022-05-26 19:22:50 +0200

    changed lambda function

--- code/lambda_function.py
+++ code/lambda_function.py
@@ -1,30 +1,29 @@
 import os
 import boto3
 import logging
-import code
-
 def lambda_handler(event, context):
 
     FUNCTION_REGION = os.environ['awsRegion']
-
+    job_id = event['CodePipeline.job']['id']
     s3Client = boto3.client('s3', region_name=FUNCTION_REGION)
+    
     code_pipeline = boto3.client('codepipeline')
     bucket_name = 'bucketsstack-codebucket-19hb395eaolra'
     objects = list(list_bucket_objects(bucket_name,s3Client))
     if objects is None:
-        code_pipeline.put_job_failure_result(jobId=event)
+        code_pipeline.put_job_failure_result(jobId=job_id)
         return 'Complete.'
+    cnt = 0
     try:
         for obj in objects:
             if obj["Key"][:-3] != 'zip':
-                print(obj["Key"])
                 res = s3Client.delete_object(Bucket=bucket_name, Key= obj["Key"])
-                print(res)
-
-        code_pipeline.put_job_success_result(jobId = event) 
+                cnt+=1
+        print(f"deleted {cnt} files")
+        code_pipeline.put_job_success_result(jobId = job_id) 
         return 'Complete.'
     except Exception as e:
-       code_pipeline.put_job_failure_result(jobId=event)
+       code_pipeline.put_job_failure_result(jobId=job_id)
        return 'Complete.'
         
     
